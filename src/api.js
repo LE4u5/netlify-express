0cceb74b68c3ce12e89276229267d424dd25fb0c
LE4u5 <tdav06@yahoo.com>
2020-07-07 02:04:26 -0400

    cors.

--- src/api.js
+++ src/api.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const PRODUCTS = require('./products')
-const cors = require('cors');
+
 
 const app = express();
 const router = express.Router();
 
-app.use(cors({origin: '*'}));
+app.use((req,res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
+
 router.get('/', (req,res) => {
     res.json( PRODUCTS );
 });
