66e834eaf9e9d4bc8d66c56b8f703acf9e6d5651
LE4u5 <tdav06@yahoo.com>
2020-07-07 01:40:03 -0400

    added cors

--- src/api.js
+++ src/api.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const PRODUCTS = require('./products')
+const cors = require('cors');
+
 const app = express();
 const router = express.Router();
 
+app.use(cors());
 router.get('/', (req,res) => {
     res.json( PRODUCTS );
 });
